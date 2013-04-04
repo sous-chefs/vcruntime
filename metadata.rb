6aca022ee0b85c35a6bd3576402a542976e14eb3
Yvo van Doorn <yvo@opscode.com>
2013-04-04 15:54:14 -0700

    update the provides and make sure it only supports windows

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,11 @@ description      'Installs/Configures vcruntime'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+%w{ windows }.each do |os|
+  supports os
+end
+
 depends "windows"
+
+provides "vcruntime::vc9"
+provides "vcruntime::vc10"
