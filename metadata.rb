2cdd3a16b2d9170891532e9be8c178abda471508
CD Jenkins <cd-jenkins@azure.can.cd>
2013-04-08 22:19:44 +0000

    Updated patch level for _thisrepo_

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yvo@opscode.com'
 license          'All rights reserved'
 description      'Installs/Configures vcruntime'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          "0.1.1"
 
 %w{ windows }.each do |os|
   supports os
