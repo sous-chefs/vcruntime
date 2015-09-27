352be42472f64985201aaa85e1b80c921c8f857a
Tim Smith <tsmith84@gmail.com>
2015-09-27 12:02:07 -0700

    Fix license metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'vcruntime'
 maintainer       'Yvo van Doorn'
 maintainer_email 'yvo@opscode.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs Microsoft Visual C++ Runtimes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.1'
