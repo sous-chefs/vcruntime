e16ac71c28e53e78b0bdc733eae85cad018344f6
Tim Smith <tsmith84@gmail.com>
2015-09-27 12:06:07 -0700

    Add Rubocop config and fixes

--- metadata.rb
+++ metadata.rb
@@ -6,20 +6,19 @@ description      'Installs Microsoft Visual C++ Runtimes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.1'
 
-%w{ windows }.each do |os|
+%w( windows ).each do |os|
   supports os
 end
 
-depends "windows"
+depends 'windows'
 
 provides 'vcruntime::vc9'
 provides 'vcruntime::vc10'
 provides 'vcruntime::vc12'
 
-recipe	"vcruntime::vc9", "Visual C++ 2008 runtimes"
-recipe	"vcruntime::vc10", "Visual C++ 2010 runtimes"
-recipe	"vcruntime::vc12", "Visual C++ 2012 runtimes"
+recipe	'vcruntime::vc9', 'Visual C++ 2008 runtimes'
+recipe	'vcruntime::vc10', 'Visual C++ 2010 runtimes'
+recipe	'vcruntime::vc12', 'Visual C++ 2012 runtimes'
 
 source_url 'https://github.com/chef-windows/vcruntime' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-windows/vcruntime/issues' if respond_to?(:issues_url)
-
