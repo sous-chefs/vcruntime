4c8ba42f208dad2bd13a8caa9953e5240bf6e2be
Taliesin Sisson <taliesins@interxion.com>
2015-10-22 09:59:48 +0100

    Update hash for files

--- attributes/vc6.rb
+++ attributes/vc6.rb
@@ -18,10 +18,10 @@
 # limitations under the License.
 
 default['vcruntime']['vc6']['x86']['6.0.2900.2180']['url']       = 'https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE'
-default['vcruntime']['vc6']['x86']['6.0.2900.2180']['sha256sum'] = '41f45a46ee56626ff2699d525bb56a3bb4718c5ca5f4fb5b3b38add64584026b'
+default['vcruntime']['vc6']['x86']['6.0.2900.2180']['sha256sum'] = '4ee4da0fe62d5fa1b5e80c6e6d88a4a2f8b3b140c35da51053d0d7b72a381d29'
 default['vcruntime']['vc6']['x86']['6.0.2900.2180']['name']      = 'Microsoft Visual C++ 2005 Redistributable - x86 6.0.2900.2180'
 default['vcruntime']['vc6']['x64']['6.0.2900.2180']['url']       = 'https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE'
-default['vcruntime']['vc6']['x64']['6.0.2900.2180']['sha256sum'] = '55bf99ad3f063165c3ee1626787b5d4c2b65b5001fef79dd58c3490369ed282f'
+default['vcruntime']['vc6']['x64']['6.0.2900.2180']['sha256sum'] = '0551a61c85b718e1fa015b0c3e3f4c4eea0637055536c00e7969286b4fa663e0'
 default['vcruntime']['vc6']['x64']['6.0.2900.2180']['name']      = 'Microsoft Visual C++ 2005 Redistributable - x64 6.0.2900.2180'
 
 default['vcruntime']['vc6']['version']  = '6.0.2900.2180'
