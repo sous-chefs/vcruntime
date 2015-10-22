1a43846f5b76e6fc53c5a4e983a7beac1c4a364e
Taliesin Sisson <taliesins@interxion.com>
2015-10-22 10:11:36 +0100

    Add 2005 Runtime

--- metadata.rb
+++ metadata.rb
@@ -12,10 +12,12 @@ end
 
 depends 'windows'
 
+provides 'vcruntime::vc6'
 provides 'vcruntime::vc9'
 provides 'vcruntime::vc10'
 provides 'vcruntime::vc12'
 
+recipe	'vcruntime::vc6', 'Visual C++ 2005 runtimes'
 recipe	'vcruntime::vc9', 'Visual C++ 2008 runtimes'
 recipe	'vcruntime::vc10', 'Visual C++ 2010 runtimes'
 recipe	'vcruntime::vc12', 'Visual C++ 2012 runtimes'
