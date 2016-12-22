4e055d24e1b9ea0ed44021cac96e47ff954f3f34
Jamie Zaharopoulos <JZaharopoulos@vigilantglobal.com>
2016-12-22 18:59:07 -0500

    fix bugs

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+puts installed_packages
+
 include_recipe 'vcruntime::vc6'
 include_recipe 'vcruntime::vc9'
 include_recipe 'vcruntime::vc10'
